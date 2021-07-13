69763fac165e13107d77e976995a7f7c6244b7c3
Md Ismail Hossain <ismailtex043@gmail.com>
2021-07-14 00:15:34 +0600

    react core concept

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState, useEffect } from "react";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <IncreaseBtn></IncreaseBtn>
+        <Users></Users>
       </header>
     </div>
   );
 }
+function IncreaseBtn() {
+  const [count, setCount] = useState(10);
+  const handleClick = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  };
+  return (
+    <div>
+      <h1>HEllo world</h1>
+      <h1>Count:{count}</h1>
+      <button onClick={handleClick}>increase</button>
+      <button onClick={() => setCount(count - 1)}>decrease</button>
+      <button onClick={() => setCount(0)}>reset</button>
+    </div>
+  );
+}
+function Users() {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUser(data));
+  }, []);
 
+  return (
+    <div>
+      <h1>hello world {user.length}</h1>
+      <ul>
+        {console.log(user)}
+        {user.map((users) => {
+          return (
+            <div>
+              <li>{users.email}</li>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
 export default App;
